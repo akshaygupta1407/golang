4cf5f4017d50c7cdbcd6d4391a93eb5eb782e919
akshaygupta1407 <akshaygupta20021407@gmail.com>
2026-03-11 16:35:51 +0530

    P3-Basic Variables

---
+++ P3-Basic Variables/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// initialize variables here
+	var smsSendingLimit int
+	var costPerSMS float64
+	var hasPermission bool
+	var username string
+
+	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
+}
+
+
+/*
+Assignment
+Initialize the variables from the print statement to int, float64, bool and string with their zero values, respectively.
+*/
